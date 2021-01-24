690a7bc638bd095d884ef6c89fc8a3d1eb523370
ssw03270 <ttd8591@gmail.com>
2021-01-24 12:05:33 +0900

    [Add] - Add find chat room function

--- Source/Main.py
+++ Source/Main.py
@@ -2,6 +2,7 @@ import SendKakao, CheckCommit
 import datetime, time
 
 while True:
+	SendKakao.enterRoom()
     if datetime.datetime.today().hour == 0 and datetime.datetime.today().minute == 0:
 	    commitData = CheckCommit.getCommit()
 		for commit in commitData:
--- Source/SendKakao.py
+++ Source/SendKakao.py
@@ -17,3 +17,14 @@ def sendReturn(hwnd):
 	time.sleep(1)
 	win32api.PostMessage(hwnd, win32con.WM_KEYUP, win32con.VK_RETURN, 0)
 
+def enterRoom():
+    hwnd = win32gui.FindWindow(None, "카카오톡")
+	hwndChild = win32gui.FindWindowEx(hwnd, None, "EVA_ChildWindow", None)
+	hwndEdit01 = win32gui.FindWindowEx(hwndChild, None, "EVA_Window", None)
+	hwndEdit02 = win32gui.FindWindowEx(hwndChild, hwndEdit01, "EVA_Window", None)
+	hwndEdit02_01 = win32gui.FindWindowEx(hwndEdit02, None, "Edit", None)
+
+	win32api.SendMessage(hwndEdit02_01, win32con.WM_SETTEXT, 0, roomTitle)
+	time.sleep(1)  # 안정성 위해 필요
+	sendReturn(hwndEdit02_01)
+	time.sleep(1)
