da032c4c7aeb750a4b90dd29c211d21d470fd645
ssw03270 <ttd8591@gmail.com>
2021-01-24 09:35:35 +0900

    [Add] - Add Main.py

---
+++ Source/Main.py
@@ -0,0 +1,11 @@
+import SendKakao, CheckCommit
+import datetime, time
+
+while True:
+    if datetime.datetime.today().hour == 0 and datetime.datetime.today().minute == 0:
+	    commitData = CheckCommit.getCommit()
+		for commit in commitData:
+			SendKakao.sendText(commit)
+	time.sleep(60)
+
+
