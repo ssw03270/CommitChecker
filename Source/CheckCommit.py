f0fcbcbe1fb5780d6e41b2b0c15437b9ded4d3b5
ssw03270 <ttd8591@gmail.com>
2021-02-14 19:43:48 +0900

    [Fix] - Fix date error

--- Source/CheckCommit.py
+++ Source/CheckCommit.py
@@ -8,14 +8,20 @@ def getCommit():
     userList = ["bluehyena", "ch4n3-yoon", "ssw03270", "mmindoong"]
     commitData = []
     for user in userList:
-        commitList = g.search_commits("author:" + user + " sort:author-date-desc")
+        allCommitList = g.search_commits("author:" + user + " sort:author-date-desc")
+        commitMessage = ""
         commitCount = 0
-        for commit in commitList:
-            if not str(getCurrentDate()) in str(commit.commit.author.date):
+        i = 20
+        for commit in allCommitList:
+            i -= 1
+            if i < 0:
                 break
-            commitCount += 1
+            if str(getCurrentDate()) in str(commit.commit.author.date):
+                commitCount += 1
+                if commitCount == 1:
+                    commitMessage = str(commit.commit.message) + " | " + str(commit.commit.author.date)
         if(commitCount > 0):
-            commitData.append(user + " : " + str(commitList[0].commit.message) + " | " + str(commitList[0].commit.author.date) + " | 그 외 " + str(commitCount - 1) + "개의 커밋을 하셨습니다.")
+            commitData.append(user + " : " + commitMessage + " | 그 외 " + str(commitCount - 1) + "개의 커밋을 하셨습니다.")
         else:
             commitData.append(user + " : 0개의 커밋을 하셨습니다. 벌금 5,000원 입니다.")
     return commitData
